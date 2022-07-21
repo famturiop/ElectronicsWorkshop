0ece4744856b203d03825eda3ba13b03a9fcc8ce
famturiop <vorobyev.seva.rules@gmail.com>
2022-07-21 18:26:07 +0300

    updated DbContext in accordance to new domain models
    added seed data in a database

--- ElectronicsWorkshop.Infrastructure/Data/ApplicationDbContext.cs
+++ ElectronicsWorkshop.Infrastructure/Data/ApplicationDbContext.cs
@@ -31,5 +31,52 @@ public class ApplicationDbContext : DbContext
                 .OnDelete(DeleteBehavior.Cascade);
         });
 
+        Seed(modelBuilder);
+    }
+
+    private void Seed(ModelBuilder modelBuilder)
+    {
+        var baseDevices = new List<BaseDevice>()
+        {
+            new BaseDevice {
+                CompositeDevices = null, Id = 1, Name = "Laptop Motherboard", Price = 20000, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 2, Name = "HD Monitor PCB", Price = 5000, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 3, Name = "Smartphone PCB", Price = 10000, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 4, Name = "Arduino", Price = 10, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 5, Name = "Raspberry Pi", Price = 100, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 6, Name = "Oscilloscope PCB", Price = 25000, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 7, Name = "VNA PCB", Price = 50000, Quantity = 100 },
+            new BaseDevice {
+                CompositeDevices = null, Id = 8, Name = "RF Amplifier PCB", Price = 1500, Quantity = 100 }
+        };
+
+        var connectors = new List<Connector>
+        {
+            new Connector {
+                CompositeDevices = null, Id = 1, Name = "FFC", Price = 11, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 2, Name = "LCEDI", Price = 9, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 3, Name = "USB", Price = 2, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 4, Name = "USB type C", Price = 2, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 5, Name = "General Pin", Price = 1, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 6, Name = "DC Power", Price = 9, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 7, Name = "BNC", Price = 6, Quantity = 100 },
+            new Connector {
+                CompositeDevices = null, Id = 8, Name = "SMA", Price = 15, Quantity = 100 }
+        };
+
+        modelBuilder.Entity<BaseDevice>().HasData(baseDevices);
+        modelBuilder.Entity<Connector>().HasData(connectors);
     }
 }
---
+++ ElectronicsWorkshop.Infrastructure/Migrations/20220720172256_changed-price-type-in-models.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectronicsWorkshop.Infrastructure.Migrations
+{
+    public partial class changedpricetypeinmodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Connectors",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "CompositeDevices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "BaseDevices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "BaseDevices",
+                columns: new[] { "Id", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Laptop Motherboard", 20000m, 100 },
+                    { 2, "HD Monitor PCB", 5000m, 100 },
+                    { 3, "Smartphone PCB", 10000m, 100 },
+                    { 4, "Arduino", 10m, 100 },
+                    { 5, "Raspberry Pi", 100m, 100 },
+                    { 6, "Oscilloscope PCB", 25000m, 100 },
+                    { 7, "VNA PCB", 50000m, 100 },
+                    { 8, "RF Amplifier PCB", 1500m, 100 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Connectors",
+                columns: new[] { "Id", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "FFC", 11m, 100 },
+                    { 2, "LCEDI", 9m, 100 },
+                    { 3, "USB", 2m, 100 },
+                    { 4, "USB type C", 2m, 100 },
+                    { 5, "General Pin", 1m, 100 },
+                    { 6, "DC Power", 9m, 100 },
+                    { 7, "BNC", 6m, 100 },
+                    { 8, "SMA", 15m, 100 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "BaseDevices",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Connectors",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Connectors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "CompositeDevices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "BaseDevices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
