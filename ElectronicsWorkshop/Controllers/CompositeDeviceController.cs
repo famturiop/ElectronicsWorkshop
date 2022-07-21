b51e45724e4574598e0132e65a2e7f5623479f8f
famturiop <vorobyev.seva.rules@gmail.com>
2022-07-21 18:29:36 +0300

    added REST controller

---
+++ ElectronicsWorkshop/Controllers/CompositeDeviceController.cs
@@ -0,0 +1,49 @@
+﻿using ElectronicsWorkshop.Core.Application.ApiModels;
+using ElectronicsWorkshop.Core.Application.ServicesInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicsWorkshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompositeDeviceController : ControllerBase
+    {
+        private readonly ICompositeDeviceService _compositeDeviceService;
+
+        public CompositeDeviceController(ICompositeDeviceService compositeDeviceService)
+        {
+            _compositeDeviceService = compositeDeviceService;
+        }
+
+        // GET api/<CompositeDeviceController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CompositeDeviceRead>> Get(int id)
+        {
+            return await _compositeDeviceService.GetCompositeDeviceAsync(id);
+        }
+
+        // POST api/<CompositeDeviceController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CompositeDeviceWrite device)
+        {
+            await _compositeDeviceService.CreateCompositeDeviceAsync(device);
+            return Ok();
+        }
+
+        // PUT api/<CompositeDeviceController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] CompositeDeviceUpdate device)
+        {
+            await _compositeDeviceService.UpdateCompositeDeviceAsync(device, id);
+            return Ok();
+        }
+
+        // DELETE api/<CompositeDeviceController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _compositeDeviceService.DeleteCompositeDeviceAsync(id);
+            return Ok();
+        }
+    }
+}
